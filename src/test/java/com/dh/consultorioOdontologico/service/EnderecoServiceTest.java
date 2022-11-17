db6ef73bbcbade27d76b15a9ed2f037ca7f2e92a
Gabriella Faria <gabisouf@gmail.com>
2022-11-17 13:37:02 -0300

    refactor: add retorno de lista na funcao buscaTodos

--- src/test/java/com/dh/consultorioOdontologico/service/EnderecoServiceTest.java
+++ src/test/java/com/dh/consultorioOdontologico/service/EnderecoServiceTest.java
@@ -2,14 +2,30 @@ package com.dh.consultorioOdontologico.service;
 
 import com.dh.consultorioOdontologico.model.Endereco;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnderecoServiceTest {
     EnderecoService enderecoService = new EnderecoService();
+
+    @Test
+    public void cadastro() throws SQLException{
+        Endereco endereco = new Endereco("Rua três", 13, "Belo Horizonte", "MG");
+        enderecoService.cadastrar(endereco);
+
+        Assertions.assertTrue(endereco.getId() > 0);
+    }
+
+    @Test
+    public void busca() throws SQLException {
+        List<Endereco> enderecos = enderecoService.buscarTodos();
+        Assertions.assertEquals(28, enderecos.size());
+    }
 
     @Test
     public void testeCompleto() throws SQLException {
@@ -23,7 +39,6 @@ class EnderecoServiceTest {
 
         enderecoService.modificar(endereco);
 
-        enderecoService.excluir(endereco);
     }
 
 }
