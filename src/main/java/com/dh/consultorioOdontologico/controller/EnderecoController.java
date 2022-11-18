29bc5d4e10d96c6301a159d15782d5cb2c48a7e3
Gabriella Faria <gabisouf@gmail.com>
2022-11-18 18:19:48 -0300

    feat: add buscarTodos in EnderecoController

--- src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
@@ -2,18 +2,21 @@ package com.dh.consultorioOdontologico.controller;
 
 import com.dh.consultorioOdontologico.model.Endereco;
 import com.dh.consultorioOdontologico.service.EnderecoService;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 import com.dh.consultorioOdontologico.model.Dentista;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
     EnderecoService enderecoService = new EnderecoService();
+
+    @GetMapping()
+    public List<Endereco> buscarTodos() throws SQLException {
+        return enderecoService.buscarTodos();
+    }
 
     @PutMapping()
     public Endereco alterarEndereco(@RequestBody Endereco endereco) throws SQLException {
