16f87d435533e4f5ea134b0b5a6dd5d8f76d4ed5
Sabrina <freibergerssb@gmail.com>
2022-11-18 13:05:50 -0300

    chore: add @PostMapping in Cosulta

--- src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
@@ -1,12 +1,23 @@
 package com.dh.consultorioOdontologico.controller;
 
+import com.dh.consultorioOdontologico.model.Consulta;
+import com.dh.consultorioOdontologico.model.Endereco;
 import com.dh.consultorioOdontologico.service.ConsultaService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/consulta")
 public class ConsultaController {
     ConsultaService consultaService = new ConsultaService();
+
+    @PostMapping()
+    public Consulta post(@RequestBody Consulta consulta) throws SQLException {
+        return consultaService.cadastrar(consulta);
+    }
 
 }
