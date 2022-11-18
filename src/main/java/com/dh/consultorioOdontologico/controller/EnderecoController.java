871a5cdc0ea181c65ccaa6b906a7ddeffb231611
Sabrina <freibergerssb@gmail.com>
2022-11-18 13:05:20 -0300

    chore: add @PostMapping in Endereco

--- src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
@@ -1,12 +1,23 @@
 package com.dh.consultorioOdontologico.controller;
 
+import com.dh.consultorioOdontologico.model.Dentista;
+import com.dh.consultorioOdontologico.model.Endereco;
 import com.dh.consultorioOdontologico.service.EnderecoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
     EnderecoService enderecoService = new EnderecoService();
+
+    @PostMapping()
+    public Endereco post(@RequestBody Endereco endereco) throws SQLException {
+        return enderecoService.cadastrar(endereco);
+    }
 
 }
