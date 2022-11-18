05d2072bbaf167472b3f9e483ab82810820142fb
Felipe Stefani <felipestefani@bol.com.br>
2022-11-17 23:17:48 -0300

    chore: add excluir to ConsultaControler

--- src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
@@ -1,12 +1,20 @@
 package com.dh.consultorioOdontologico.controller;
 
+import com.dh.consultorioOdontologico.model.Consulta;
 import com.dh.consultorioOdontologico.service.ConsultaService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/consulta")
 public class ConsultaController {
     ConsultaService consultaService = new ConsultaService();
 
+    @DeleteMapping()
+    public void excluirConsulta(Consulta consulta) throws SQLException {
+        consultaService.excluir(consulta);
+    }
 }
