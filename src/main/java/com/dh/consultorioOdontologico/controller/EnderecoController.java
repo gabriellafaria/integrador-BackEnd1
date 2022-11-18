8fb9a1b9d6eb98bbf171d7a79acd8d0162061c3e
Felipe Stefani <felipestefani@bol.com.br>
2022-11-17 23:18:58 -0300

    chore: add excluir to EnderecoController

--- src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
@@ -1,12 +1,21 @@
 package com.dh.consultorioOdontologico.controller;
 
+import com.dh.consultorioOdontologico.model.Endereco;
 import com.dh.consultorioOdontologico.service.EnderecoService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
     EnderecoService enderecoService = new EnderecoService();
 
+    @DeleteMapping()
+    public void excluirEndereco(@RequestBody Endereco endereco) throws SQLException {
+        enderecoService.excluir(endereco);
+    }
 }
