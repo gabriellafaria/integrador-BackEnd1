0ff8c3f295e02ab7133d6912ad170ac85f5ce227
Felipe Stefani <felipestefani@bol.com.br>
2022-11-17 23:19:59 -0300

    chore: add exlcuir to PacienteController

--- src/main/java/com/dh/consultorioOdontologico/controller/PacienteController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/PacienteController.java
@@ -1,12 +1,22 @@
 package com.dh.consultorioOdontologico.controller;
 
+import com.dh.consultorioOdontologico.model.Paciente;
 import com.dh.consultorioOdontologico.service.PacienteService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
     PacienteService pacienteService = new PacienteService();
+
+    @DeleteMapping()
+    public void excluirPaciente(@RequestBody Paciente paciente) throws SQLException {
+        pacienteService.excluir(paciente);
+    }
 
 }
