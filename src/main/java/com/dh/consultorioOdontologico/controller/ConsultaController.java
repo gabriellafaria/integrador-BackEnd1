13fc36e5c482f5905b622dd3f626adc50b026189
Mariana Orsi <marianaorsi95@gmail.com>
2022-11-27 21:07:48 -0300

    Adicionando no pom.xml:
    <dependency>
            	<groupId>com.h2database</groupId>
                <artifactId>h2</artifactId>
                <scope>runtime</scope>
    </dependency>

--- src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
@@ -1,22 +1,28 @@
-//package com.dh.consultorioOdontologico.controller;
-//
-//import com.dh.consultorioOdontologico.entity.Consulta;
-//import com.dh.consultorioOdontologico.service.ConsultaService;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.sql.SQLException;
-//
-//@RestController
-//@RequestMapping("/consulta")
-//public class ConsultaController {
-//    ConsultaService consultaService = new ConsultaService();
-//
-//    @PostMapping()
-//    public Consulta post(@RequestBody Consulta consulta) throws SQLException {
-//        return consultaService.cadastrar(consulta);
-//    }
-//
-//}
+package com.dh.consultorioOdontologico.controller;
+
+import com.dh.consultorioOdontologico.entity.Consulta;
+import com.dh.consultorioOdontologico.entity.dto.ConsultaDTO;
+import com.dh.consultorioOdontologico.service.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consulta")
+
+public class ConsultaController {
+    @Autowired
+    ConsultaService service;
+
+    @PostMapping()
+    public ResponseEntity salvar(@RequestBody Consulta consulta){
+        return service.salvar(consulta);
+    }
+
+    @GetMapping()
+    public List<ConsultaDTO> buscarConsultas(){
+        return service.buscar();
+    }
+}
---
+++ src/main/java/com/dh/consultorioOdontologico/repository/ConsultaRepository.java
@@ -0,0 +1,10 @@
+package com.dh.consultorioOdontologico.repository;
+
+import com.dh.consultorioOdontologico.entity.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository <Consulta, Long>{
+
+}
