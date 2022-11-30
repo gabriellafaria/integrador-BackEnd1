4207dd311586d63569746c1aca7fafad9018b61d
Gabriella Faria <gabisouf@gmail.com>
2022-11-29 22:41:10 -0300

    feat: add object Endereco in Paciente, @OneToOne relationship

--- src/main/java/com/dh/consultorioOdontologico/entity/dto/PacienteDTO.java
+++ src/main/java/com/dh/consultorioOdontologico/entity/dto/PacienteDTO.java
@@ -1,12 +1,17 @@
 package com.dh.consultorioOdontologico.entity.dto;
 
+import com.dh.consultorioOdontologico.entity.Endereco;
+import com.dh.consultorioOdontologico.service.EnderecoService;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -16,7 +21,12 @@ import java.sql.Timestamp;
 public class PacienteDTO {
     private String nome;
     private String sobrenome;
-    private int idEndereco;
+    private EnderecoDTO enderecoDTO;
     private String rg;
     private Timestamp dataRegistro;
+
+   public EnderecoDTO setEndereco(Endereco endereco){
+        ObjectMapper mapper = new ObjectMapper();
+        return this.enderecoDTO = mapper.convertValue(endereco, EnderecoDTO.class);
+   }
 }
