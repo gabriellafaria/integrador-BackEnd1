9869d52d85fd7dea8a8087a07b85489914dd845d
Sabrina <freibergerssb@gmail.com>
2022-11-30 21:51:04 -0300

    Feat: add validations

--- src/main/java/com/dh/consultorioOdontologico/entity/Dentista.java
+++ src/main/java/com/dh/consultorioOdontologico/entity/Dentista.java
@@ -1,18 +1,24 @@
 package com.dh.consultorioOdontologico.entity;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-//@Table(name="Dentista")  -> não precisa porque o nome da tabela é o mesmo da classe
+
 public class Dentista {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+
     @Column(nullable = false, length = 100)
     private String nome;
     @Column(nullable = false, length = 100)
--- src/main/java/com/dh/consultorioOdontologico/service/DentistaService.java
+++ src/main/java/com/dh/consultorioOdontologico/service/DentistaService.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Validated
 @Service
 public class DentistaService {
     @Autowired
