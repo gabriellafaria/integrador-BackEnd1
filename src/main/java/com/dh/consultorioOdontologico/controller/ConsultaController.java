26291d47998354acceea4d235dce2c4b72ecfbb9
Felipe Stefani <felipestefani92@gmail.com>
2022-12-01 15:58:44 -0300

    refactor: consulta save with dto

--- src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/ConsultaController.java
@@ -1,11 +1,9 @@
 package com.dh.consultorioOdontologico.controller;
 
-import com.dh.consultorioOdontologico.entity.Consulta;
 import com.dh.consultorioOdontologico.entity.dto.ConsultaDTO;
 import com.dh.consultorioOdontologico.service.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,8 +16,8 @@ public class ConsultaController {
     ConsultaService consultaService;
 
     @PostMapping()
-    public ResponseEntity salvar(@RequestBody @Valid Consulta consulta){
-        return consultaService.salvar(consulta);
+    public ResponseEntity salvarConsulta(@RequestBody @Valid ConsultaDTO consultaDTO){
+        return consultaService.salvar(consultaDTO);
     }
 
     @GetMapping("/buscar_todas_consultas")
@@ -28,11 +26,6 @@ public class ConsultaController {
     }
 
     @DeleteMapping
-    public ResponseEntity deletar(@RequestBody @Valid Consulta consulta) { return consultaService.deletar(consulta); }
-
-//    @GetMapping("/buscar_por_rg/{rgPaciente}")
-//    public List<ConsultaDTO> buscarConsultaPorRg(@PathVariable String rgPaciente){
-//        return service.findConsultByRg(rgPaciente);
-//    }
+    public ResponseEntity deletar(@RequestBody @Valid ConsultaDTO consultaDTO) { return consultaService.deletar(consultaDTO); }
 
 }
