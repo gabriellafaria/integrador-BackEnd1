c28aa2039e1bcab2db0ce99ef840d3017199731c
Everton Silva <everton.p.silva@outlook.com>
2022-12-03 00:12:22 -0300

    refactor: including findById at EnderecoRepository.

--- src/main/java/com/dh/consultorioOdontologico/repository/EnderecoRepository.java
+++ src/main/java/com/dh/consultorioOdontologico/repository/EnderecoRepository.java
@@ -3,5 +3,8 @@ package com.dh.consultorioOdontologico.repository;
 import com.dh.consultorioOdontologico.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    Optional<Endereco> findById(Long id);
 }
