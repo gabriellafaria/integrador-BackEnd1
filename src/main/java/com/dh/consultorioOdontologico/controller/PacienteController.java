595d193b1fa9ddd92cbce656f6963c0f4c021979
Gabriella Faria <gabisouf@gmail.com>
2022-12-04 16:36:37 -0300

    refactor: removing commented code

--- src/main/java/com/dh/consultorioOdontologico/controller/PacienteController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/PacienteController.java
@@ -45,42 +45,4 @@ public class PacienteController {
     public ResponseEntity alterarTudo(@RequestBody @Valid PacienteDTO pacienteDTO){
         return pacienteService.alterarTudo(pacienteDTO);
     }
-/*    @Autowired
-    private ModelMapper modelMapper;
-
-    @GetMapping()
-    public List<Paciente> buscarTodos() throws SQLException {
-        return pacienteService.buscarTodos();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Paciente> buscarPacientePorId(@PathVariable("id") int id) throws SQLException {
-        return pacienteService.buscarPorId(id);
-    }
-
-    @DeleteMapping()
-    public void excluirPaciente(@RequestBody Paciente paciente) throws SQLException {
-        pacienteService.excluir(paciente);
-
-    }
-
-    @PutMapping()
-    public Paciente alterarPaciente(@RequestBody Paciente paciente) throws SQLException {  //Felipe comment: acho que nao precisa passar o id como PathVariable, pq podemos retirar o id do paciente direto
-        return pacienteService.modificar(paciente);
-    }
-
-    @PostMapping()
-    public Paciente post(@RequestBody Paciente paciente) throws SQLException {
-        return pacienteService.cadastrar(paciente);
-    }
-
-
-    @PatchMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void alterarPacienteParcial(@PathVariable("id") int id, @RequestBody Paciente paciente) throws SQLException {
-            pacienteService.buscarPorId(id).map(pacienteBase -> {
-            modelMapper.map(paciente, pacienteBase);
-            return Void.TYPE;
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado"));
-    }*/
 }
