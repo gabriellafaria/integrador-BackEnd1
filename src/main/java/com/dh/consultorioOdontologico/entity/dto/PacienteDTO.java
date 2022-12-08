b1f199ec5968cc0ca5cf0eac6b07bd6c34d7af97
Gabriella Faria <gabisouf@gmail.com>
2022-12-07 21:05:28 -0300

    refactor: add validations in PacienteDTO

--- src/main/java/com/dh/consultorioOdontologico/entity/dto/PacienteDTO.java
+++ src/main/java/com/dh/consultorioOdontologico/entity/dto/PacienteDTO.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
@@ -21,16 +22,18 @@ import java.sql.Timestamp;
 public class PacienteDTO {
     @NotBlank
     private String nome;
+    @NotBlank
     private String sobrenome;
+    @NotBlank
     @Size(min= 6, max = 15, message = "O tamanho do campo deve ser maior que 6 e menor que 15.")
     private String rg;
-
-    private EnderecoDTO enderecoDTO;
+    @NotNull
+    private EnderecoDTO endereco;
 
     private Timestamp dataRegistro;
 
    public EnderecoDTO setEnderecoDTO(Endereco endereco){
         ObjectMapper mapper = new ObjectMapper();
-        return this.enderecoDTO = mapper.convertValue(endereco, EnderecoDTO.class);
+        return this.endereco = mapper.convertValue(endereco, EnderecoDTO.class);
    }
 }
