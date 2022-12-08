96d387d1bcd6280cc367fe472762fead038ae9cd
Sabrina <freibergerssb@gmail.com>
2022-12-07 23:12:00 -0300

    Fix: bugs in Exceptions class

--- src/main/java/com/dh/consultorioOdontologico/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dh/consultorioOdontologico/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.dh.consultorioOdontologico.exception.handler;
+import com.dh.consultorioOdontologico.exception.Exceptions;
 import com.dh.consultorioOdontologico.exception.ResourceNotFoundException;
-import com.dh.consultorioOdontologico.exception.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({exceptions.class})
-    public ResponseEntity<String> errorResourceNotFound(exceptions exception) {
+    @ExceptionHandler({Exceptions.class})
+    public ResponseEntity<String> errorResourceNotFound(Exceptions exception) {
         return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
