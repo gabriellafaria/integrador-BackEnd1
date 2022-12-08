fa30f1907fa82e41fffda2ea4746a2e22d44f5df
Gabriella Faria <gabisouf@gmail.com>
2022-12-07 21:07:29 -0300

    refactor: altering PacienteService to enable tests

--- src/main/java/com/dh/consultorioOdontologico/service/PacienteService.java
+++ src/main/java/com/dh/consultorioOdontologico/service/PacienteService.java
@@ -2,15 +2,11 @@ package com.dh.consultorioOdontologico.service;
 
 import com.dh.consultorioOdontologico.entity.Endereco;
 import com.dh.consultorioOdontologico.entity.Paciente;
-import com.dh.consultorioOdontologico.entity.dto.EnderecoDTO;
 import com.dh.consultorioOdontologico.entity.dto.PacienteDTO;
-import com.dh.consultorioOdontologico.exception.NotFoundException;
 import com.dh.consultorioOdontologico.repository.PacienteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
@@ -24,9 +20,6 @@ import java.util.Optional;
 public class PacienteService {
     @Autowired
     PacienteRepository pacienteRepository;
-
-    @Autowired
-    EnderecoService enderecoService;
 
     static final Logger logger = Logger.getLogger(PacienteService.class);
 
@@ -43,105 +36,99 @@ public class PacienteService {
         return pacienteDTOList;
     }
 
-    public Paciente salvar(PacienteDTO pacienteDTO){
+    public PacienteDTO salvar(PacienteDTO pacienteDTO){
         ObjectMapper mapper = new ObjectMapper();
         logger.info("Iniciando operação para salvar o paciente.");
-        Endereco endereco = mapper.convertValue(pacienteDTO.getEnderecoDTO(), Endereco.class);
-        /*EnderecoDTO enderecoDTO = pacienteDTO.getEnderecoDTO();
-        enderecoService.salvarEndereco(enderecoDTO);*/
+        Endereco endereco = mapper.convertValue(pacienteDTO.getEndereco(), Endereco.class);
         Paciente paciente = mapper.convertValue(pacienteDTO, Paciente.class);
         paciente.setDataRegistro(Timestamp.from(Instant.now()));
         paciente.setEndereco(endereco);
         Paciente pacienteSalvo = pacienteRepository.save(paciente);
         logger.info("Paciente " + pacienteSalvo.getNome() + " salvo com sucesso.");
-        return pacienteSalvo;
+        return pacienteDTO;
     }
 
-    public ResponseEntity deletar(String  rg){
+    public Optional<Paciente> deletar(String  rg){
         logger.info("Iniciada operação para deletar o paciente com o Rg " + rg);
        Optional<Paciente> paciente = Optional.ofNullable(pacienteRepository.findByRg(rg));
-       if(paciente.isEmpty()) {
+       if(paciente.isEmpty())
            logger.error("Erro ao excluir o paciente. Não foi localizado nenhum cadastro com o rg " + rg);
-           return new ResponseEntity<>("Id do paciente não existe!", HttpStatus.BAD_REQUEST);
-       }
        pacienteRepository.deleteById(paciente.get().getId());
         logger.info("Paciente deletado!");
-        return new ResponseEntity("Paciente delatado com sucesso!", HttpStatus.OK);
+        return paciente;
     }
 
-    public ResponseEntity buscarPorRg(String rg) {
+    public PacienteDTO buscarPorRg(String rg) {
         logger.info("iniciando operação para buscar o paciente com o rg " + rg);
         ObjectMapper mapper = new ObjectMapper();
         Optional<Paciente> paciente = Optional.ofNullable(pacienteRepository.findByRg(rg));
         if(paciente.isEmpty()) {
             logger.error("Paciente com o rg " + rg + " não localizado.");
-            return new ResponseEntity("Paciente com o rg " + rg + " não encontrado", HttpStatus.BAD_REQUEST);
         }
         PacienteDTO pacienteDTO = mapper.convertValue(paciente.get(), PacienteDTO.class);
         logger.info("Paciente com o rg " + rg + " localizado com sucesso.");
-        return new ResponseEntity(pacienteDTO, HttpStatus.OK);
+        return pacienteDTO;
     }
 
-    public ResponseEntity alterarParcialmente(PacienteDTO pacienteDTO){
+    public PacienteDTO alterarParcialmente(PacienteDTO pacienteDTO){
         ObjectMapper mapper = new ObjectMapper();
         logger.info("Iniciando operação para alterar parcialmente o paciente.");
         Optional<Paciente> pacienteOp = Optional.ofNullable(pacienteRepository.findByRg(pacienteDTO.getRg()));
         if(pacienteOp.isEmpty())
-            return new ResponseEntity("Não existe o paciente com o rg " + pacienteDTO.getRg(), HttpStatus.NOT_FOUND);
+            logger.error("Paciente com o rg " + pacienteDTO.getRg() + " não encontrado.");
         Paciente paciente = pacienteOp.get();
         if(pacienteDTO.getNome() != null)
             paciente.setNome(pacienteDTO.getNome());
          else
             paciente.getNome();
-
         if(pacienteDTO.getSobrenome() != null)
             paciente.setSobrenome(pacienteDTO.getSobrenome());
         else
             paciente.getSobrenome();
-        if(pacienteDTO.getEnderecoDTO().getRua() != null)
-            paciente.getEndereco().setRua(pacienteDTO.getEnderecoDTO().getRua());
+        if(pacienteDTO.getEndereco().getRua() != null)
+            paciente.getEndereco().setRua(pacienteDTO.getEndereco().getRua());
         else
             paciente.getEndereco().getRua();
-        if(pacienteDTO.getEnderecoDTO().getNumero() != 0)
-            paciente.getEndereco().setNumero(pacienteDTO.getEnderecoDTO().getNumero());
+        if(pacienteDTO.getEndereco().getNumero() != 0)
+            paciente.getEndereco().setNumero(pacienteDTO.getEndereco().getNumero());
         else
             paciente.getEndereco().getNumero();
-        if(pacienteDTO.getEnderecoDTO().getCidade() != null)
-            paciente.getEndereco().setCidade(pacienteDTO.getEnderecoDTO().getCidade());
+        if(pacienteDTO.getEndereco().getCidade() != null)
+            paciente.getEndereco().setCidade(pacienteDTO.getEndereco().getCidade());
         else
             paciente.getEndereco().getCidade();
-        if(pacienteDTO.getEnderecoDTO().getEstado() != null)
-            paciente.getEndereco().setEstado(pacienteDTO.getEnderecoDTO().getEstado());
+        if(pacienteDTO.getEndereco().getEstado() != null)
+            paciente.getEndereco().setEstado(pacienteDTO.getEndereco().getEstado());
         else
             paciente.getEndereco().getEstado();
 
         PacienteDTO pacienteAlt = mapper.convertValue(pacienteRepository.save(paciente), PacienteDTO.class);
         logger.info("Paciente alterado com sucesso!");
-        return new ResponseEntity(pacienteAlt, HttpStatus.CREATED);
+        return pacienteAlt;
     }
 
-    public ResponseEntity alterarTudo(PacienteDTO pacienteDTO){
+    public PacienteDTO alterarTudo(PacienteDTO pacienteDTO){
         ObjectMapper mapper = new ObjectMapper();
         logger.info("Iniciando operação para alterar parcialmente o paciente.");
         Optional<Paciente> pacienteOp = Optional.ofNullable(pacienteRepository.findByRg(pacienteDTO.getRg()));
         if(pacienteOp.isEmpty())
-            return new ResponseEntity("Não existe o paciente com o rg " + pacienteDTO.getRg(), HttpStatus.NOT_FOUND);
+            logger.error("Não existe o paciente com o rg " + pacienteDTO.getRg());
         Paciente paciente = pacienteOp.get();
         if(pacienteDTO.getNome() != null)
             paciente.setNome(pacienteDTO.getNome());
         if(pacienteDTO.getSobrenome() != null)
             paciente.setSobrenome(pacienteDTO.getSobrenome());
-        if(pacienteDTO.getEnderecoDTO().getRua() != null)
-            paciente.getEndereco().setRua(pacienteDTO.getEnderecoDTO().getRua());
-        if(pacienteDTO.getEnderecoDTO().getNumero() != 0)
-            paciente.getEndereco().setNumero(pacienteDTO.getEnderecoDTO().getNumero());
-        if(pacienteDTO.getEnderecoDTO().getCidade() != null)
-            paciente.getEndereco().setCidade(pacienteDTO.getEnderecoDTO().getCidade());
-        if(pacienteDTO.getEnderecoDTO().getEstado() != null)
-            paciente.getEndereco().setEstado(pacienteDTO.getEnderecoDTO().getEstado());
+        if(pacienteDTO.getEndereco() != null)
+            paciente.getEndereco().setRua(pacienteDTO.getEndereco().getRua());
+        if(pacienteDTO.getEndereco().getNumero() != 0)
+            paciente.getEndereco().setNumero(pacienteDTO.getEndereco().getNumero());
+        if(pacienteDTO.getEndereco().getCidade() != null)
+            paciente.getEndereco().setCidade(pacienteDTO.getEndereco().getCidade());
+        if(pacienteDTO.getEndereco().getEstado() != null)
+            paciente.getEndereco().setEstado(pacienteDTO.getEndereco().getEstado());
 
         PacienteDTO pacienteAlt = mapper.convertValue(pacienteRepository.save(paciente), PacienteDTO.class);
         logger.info("Paciente alterado com sucesso!");
-        return new ResponseEntity(pacienteAlt, HttpStatus.CREATED);
+        return pacienteAlt;
     }
 }
