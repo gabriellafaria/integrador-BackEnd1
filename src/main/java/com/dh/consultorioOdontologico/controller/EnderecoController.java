dfe5390db2a921a2f2143d0f31157506c4235e66
Everton Silva <everton.p.silva@outlook.com>
2022-12-09 23:23:10 -0300

    refactor: add response entity in methods

--- src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/EnderecoController.java
@@ -2,7 +2,9 @@ package com.dh.consultorioOdontologico.controller;
 
 import com.dh.consultorioOdontologico.entity.dto.EnderecoDTO;
 import com.dh.consultorioOdontologico.entity.Endereco;
+import com.dh.consultorioOdontologico.exception.Exceptions;
 import com.dh.consultorioOdontologico.service.EnderecoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin("*")
 @RestController
@@ -21,29 +25,67 @@ public class EnderecoController {
     EnderecoService enderecoService;
 
     @PostMapping()
-    public ResponseEntity salvar(@RequestBody EnderecoDTO endereco) {
-        return enderecoService.salvarEndereco(endereco);
+    public ResponseEntity salvar(@RequestBody @Valid EnderecoDTO endereco) throws Exceptions {
+        try {
+            Endereco enderecoSalvo = enderecoService.salvar(endereco);
+            return new ResponseEntity("Endereco " + enderecoSalvo.getId() + " cadastrado com sucesso.", HttpStatus.CREATED);
+        }catch (Exception e) {
+            throw new Exceptions("Não foi possível cadastrar o endereço.");
+        }
     }
 
     @GetMapping("/buscar")
-    public List<EnderecoDTO> buscarEnderecos(){
-        return enderecoService.buscarTodosEnderecos();
+    public ResponseEntity buscarEnderecos(){
+        try {
+            List<EnderecoDTO> buscarTodosEnderecos = enderecoService.buscarTodosEnderecos();
+            return new ResponseEntity(buscarTodosEnderecos, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity("Não foi possível localizar os endereços.", HttpStatus.NOT_FOUND);
+
+        }
     }
 
     @GetMapping()
     public ResponseEntity buscarPorId(@RequestParam("id") Long id){
-        return enderecoService.buscarEnderecoPorId(id);
+        try {
+            Optional<Endereco> pesquisarPorId = enderecoService.buscarEnderecoPorId(id);
+            return new ResponseEntity(pesquisarPorId, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity("O endereço informado não existe.", HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PatchMapping()
-    public ResponseEntity alterarParcial(@RequestBody EnderecoDTO enderecoDTO){
-        return enderecoService.alterarParcial(enderecoDTO);
+    public ResponseEntity alterarParcial(@RequestBody @Valid EnderecoDTO enderecoDTO){
+        try {
+        EnderecoDTO enderecoParc = enderecoService.alterarParcial(enderecoDTO);
+        return new ResponseEntity(enderecoParc, HttpStatus.CREATED);
+        //return new ResponseEntity("Endereço com o id " + enderecoParc.getId() + "alterado parcialmente com sucesso.", HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity("Não foi possível alterar parcialmente o endereço ", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity alterarTotal(@RequestBody @Valid EnderecoDTO enderecoDTO){
+        try{
+        EnderecoDTO enderecoAlterado = enderecoService.alterarTotal(enderecoDTO);
+        return new ResponseEntity(enderecoAlterado, HttpStatus.OK);
+        //return new ResponseEntity("Endereço atualizado com sucesso | Rua: " + enderecoAlterado.getRua() + " - Numero: " + enderecoAlterado.getNumero() + " - Cidade: " + enderecoAlterado.getCidade() + " - Estado: " + enderecoAlterado.getEstado(), HttpStatus.CREATED);
+        }catch (Exception e) {
+            return new ResponseEntity("Não foi possível atualizar o endereço.", HttpStatus.NOT_FOUND);
+        }
     }
 
 
     @DeleteMapping()
     public ResponseEntity deletarEndereco(@RequestParam("id") Long id){
-        return enderecoService.deletarEndereco(id);
+        try {
+            Optional<Endereco> endereco = enderecoService.deletarEndereco(id);
+            return new ResponseEntity("Endereço deletado com sucesso.", HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity("Não foi possível deletar o endereço.", HttpStatus.NOT_FOUND);
+        }
     }
 
 }
