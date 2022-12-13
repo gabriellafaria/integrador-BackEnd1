a0772157bdbb0484cdc6581403ed80c83124075a
Gabriella Faria <gabisouf@gmail.com>
2022-12-12 22:28:38 -0300

    feat: add @CrossOrigin in AutenticacaoController

--- src/main/java/com/dh/consultorioOdontologico/controller/AutenticacaoController.java
+++ src/main/java/com/dh/consultorioOdontologico/controller/AutenticacaoController.java
@@ -10,13 +10,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
